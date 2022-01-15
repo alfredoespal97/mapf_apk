a1e55a2a2c8cfb58fa0e3268e82ba6cc3f58ba1d
Alfredo R Espinosa <alfredorespal@gmail.com>
2022-01-15 13:00:45 -0500

    change icon, scanner and home

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,3 +1,4 @@
+import 'package:barcode_scan2/barcode_scan2.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mapf/views/galeria.dart';
@@ -16,76 +17,88 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  final titles = ["Galería de Imágenes", "QR Scanner", "Recorrido Virtual", "Información"];
+  final titles = [
+    "Galería de Imágenes",
+    "QR Scanner",
+    "Recorrido Virtual",
+    "Información"
+  ];
   final subtitles = [
     "En esta sección encontrarás bellas imágenes en las que podrás regresar al pasado para disfrutar de esta maravillosa construcción.",
     "Te invitamos a conocer más acerca de la historia, la arquitectura y nuestros horios de visitas. Disfrute conociendo más acerca de la cultura cienfueguera.",
     "Te invitamos a conocer más acerca de la historia, la arquitectura y nuestros horios de visitas. Disfrute conociendo más acerca de la cultura cienfueguera.",
     "Te invitamos a conocer más acerca de la historia, la arquitectura y nuestros horarios de visitas. Disfrute conociendo más acerca de la cultura cienfueguera.",
   ];
-  final icons = [Icons.image, Icons.qr_code_scanner, Icons.remove_red_eye,Icons.info];
-
-
+  final icons = [
+    Icons.image,
+    Icons.qr_code_scanner,
+    Icons.remove_red_eye,
+    Icons.info
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body:Column(
-          children: [
-            Container(
+      body: Column(
+        children: [
+          Container(
               //padding: const EdgeInsets.all(15.0),
               child: ClipRRect(
-              borderRadius: BorderRadius.only(bottomRight: Radius.circular(50)),
-                child:  Image.asset('assets/foto_principal.jpg')
-              )
-            ),
-            Expanded(
-              child: ListView.builder(
-                  itemCount: titles.length,
-                  padding: EdgeInsets.all(20.0),
-                  itemBuilder: (context, index){
-                    return  Container(
-                      padding: EdgeInsets.all(10),
-                      margin: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).primaryColor,
-                        borderRadius: BorderRadius.all(Radius.circular(30)),
-                      ),
-                      child: ListTile(
-                          onTap: (){
-                            switch (index){
-                              case 0:
-                                Get.to(GaleriaPage());
-                                break;
-                              case 1:
-                                Get.to(ScannerPage());
-                                break;
-                              case 2:
-                                Get.to(VirtualPage());
-                                break;
-                              case 3:
-                                Get.to(InformationPage());
-                                break;
-                            }
-                          },
-                          title: Text(titles[index],style: TextStyle(color: Colors.white,fontSize: 25),),
-                          subtitle: Text(subtitles[index],style: TextStyle(color: Colors.white)),
-                          leading: CircleAvatar(
-                            backgroundColor: Theme.of(context).primaryColor,
-                            child:Icon(icons[index]) ,
-                          )
-                      ),
-                    );
-                  }
-              ),
-            ),
-          ],
-        ),
-
-      );
+                  borderRadius:
+                      BorderRadius.only(bottomRight: Radius.circular(50)),
+                  child: Image.asset('assets/foto_principal.jpg'))),
+          Expanded(
+            child: ListView.builder(
+                itemCount: titles.length,
+                padding: EdgeInsets.all(20.0),
+                itemBuilder: (context, index) {
+                  return Container(
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                    ),
+                    child: ListTile(
+                        onTap: () {
+                          switch (index) {
+                            case 0:
+                              Get.to(GaleriaPage());
+                              break;
+                            case 1:
+                              handleQrScanner();
+                              break;
+                            case 2:
+                              Get.to(VirtualPage());
+                              break;
+                            case 3:
+                              Get.to(InformationPage());
+                              break;
+                          }
+                        },
+                        title: Text(
+                          titles[index],
+                          style: TextStyle(color: Colors.white, fontSize: 25),
+                        ),
+                        subtitle: Text(subtitles[index],
+                            style: TextStyle(color: Colors.white)),
+                        leading: CircleAvatar(
+                          backgroundColor: Theme.of(context).primaryColor,
+                          child: Icon(icons[index]),
+                        )),
+                  );
+                }),
+          ),
+        ],
+      ),
+    );
   }
 
+  handleQrScanner() async {
+    var result = await BarcodeScanner.scan();
+
+    Get.to(ScannerPage(), arguments: result.rawContent);
+  }
 }
 
 class Splash extends StatelessWidget {
@@ -97,7 +110,7 @@ class Splash extends StatelessWidget {
         MediaQuery.of(context).platformBrightness == Brightness.light;
     return Scaffold(
       backgroundColor:
-      lightMode ? const Color(0xffe1f5fe) : const Color(0xff042a49),
+          lightMode ? const Color(0xffe1f5fe) : const Color(0xff042a49),
       body: Center(
           child: lightMode
               ? Image.asset('assets/pantalla_inicial.png')
